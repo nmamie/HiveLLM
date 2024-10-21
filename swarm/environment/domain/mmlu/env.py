small bug fixes related to GPU and different active nodes for policy gradient

swarm/environment/domain/mmlu/env.py
@@ -125,8 +125,7 @@ class GymWrapper:
                     
         # next_state = np.expand_dims(next_state, axis=0)
         return next_state, self.node2idx[self.current_node_id], reward, terminate, final_answers
-        
-        
+
         
     async def val_reset(self):
         """Method overloads reset

swarm/environment/domain/mmlu/evaluator.py
@@ -61,7 +61,7 @@ class Evaluator():
             
         self.utilities = []
             
-        self.device = torch.device(args.gpu_id if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device("cuda:" + str(args.gpu_id) if torch.cuda.is_available() else "cpu")
         
         self.args = args
 

swarm/optimizer/edge_optimizer/evo_ppo/algos/ddqn.py
@@ -15,7 +15,7 @@ class DDQN(object):
         self.gamma = args.gamma
         self.tau = args.tau
         self.alpha = args.alpha
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:" + str(args.gpu_id) if torch.cuda.is_available() else "cpu")
 
         self.actor = model_constructor.make_model('CategoricalPolicy').to(device=self.device)
         self.actor_optim = Adam(self.actor.parameters(), lr=args.actor_lr)
@@ -27,7 +27,7 @@ class DDQN(object):
         self.softmax = torch.nn.Softmax(dim=1)
         self.num_updates = 0
 
-    def update_parameters(self, state_batch, next_state_batch, action_batch, reward_batch, active_node_batch, step_batch, edge_index_batch, done_batch, batch_size, node_feature_size, num_nodes, num_edges):
+    def update_parameters(self, state_batch, next_state_batch, action_batch, reward_batch, active_node_batch, next_active_node_batch, step_batch, edge_index_batch, done_batch, batch_size, node_feature_size, num_nodes, num_edges):
         state_batch = torch.reshape(state_batch, (batch_size, num_nodes, node_feature_size))
         next_state_batch = torch.reshape(next_state_batch, (batch_size, num_nodes, node_feature_size))
         # edge_index_batch = torch.reshape(edge_index_batch, (batch_size, 2, num_edges))
@@ -37,6 +37,7 @@ class DDQN(object):
         action_batch=action_batch.to(self.device)
         reward_batch=reward_batch.to(self.device)
         active_node_batch=active_node_batch.to(self.device)
+        next_active_node_batch=next_active_node_batch.to(self.device)
         step_batch=step_batch.to(self.device)
         edge_index_batch=edge_index_batch.to(self.device)
         done_batch=done_batch.to(self.device)
@@ -50,7 +51,7 @@ class DDQN(object):
                 # Example edge index for this graph
                 edge_index = edge_index_batch[i].long()
                 
-                active_node = active_node_batch[i].long()
+                active_node = next_active_node_batch[i].long()
                 
                 step = step_batch[i].long()
 

swarm/optimizer/edge_optimizer/evo_ppo/algos/erl_trainer.py
@@ -28,7 +28,7 @@ class ERL_Trainer:
 		self.num_edges = num_edges
 		self.pruned_nodes = []
 		self.env_constructor = env_constructor
-		self.device = torch.device(args.gpu_id if torch.cuda.is_available() else "cpu")
+		self.device = torch.device("cuda:" + str(args.gpu_id) if torch.cuda.is_available() else "cpu")
   
   		#Save best policy
 		self.best_policy = model_constructor.make_model(self.policy_string)
@@ -118,8 +118,8 @@ class ERL_Trainer:
 		############# UPDATE PARAMS USING GRADIENT DESCENT ##########
 		if self.replay_buffer.__len__() > self.args.learning_start: ###BURN IN PERIOD
 			for _ in range(int(self.gen_frames * self.args.gradperstep)):
-				s, ns, a, r, n, t, e, done = self.replay_buffer.sample(self.args.batch_size)
-				self.learner.update_parameters(s, ns, a, r, n, t, e, done, self.args.batch_size, self.args.node_feature_size, self.num_nodes, self.num_edges)
+				s, ns, a, r, n, nn, t, e, done = self.replay_buffer.sample(self.args.batch_size)
+				self.learner.update_parameters(s, ns, a, r, n, nn, t, e, done, self.args.batch_size, self.args.node_feature_size, self.num_nodes, self.num_edges)
 
 			self.gen_frames = 0
 
@@ -279,7 +279,7 @@ class ERL_Trainer:
 			while not done:
 				state = state.to(self.device)
 				edge_index = edge_index.to(self.device)
-				action = self.best_policy.clean_action(state, edge_index, active_node_idx, sentence, step=steps, pruned_nodes=self.pruned_nodes)
+				action = self.best_policy.clean_action(state, edge_index, active_node_idx, sentence, step=steps, pruned_nodes=[])
 				state, active_node_idx, reward, done, final_answers = await env.val_step(action, record, state, edge_index)
 				steps += 1
 			raw_answer = final_answers[0]

swarm/optimizer/edge_optimizer/evo_ppo/core/buffer.py
@@ -21,6 +21,7 @@ class Buffer():
         self.a = []
         self.r = []
         self.n = []
+        self.nn = []
         self.t = []
         self.e = []
         self.done = []
@@ -34,9 +35,10 @@ class Buffer():
             self.a.append(torch.Tensor(exp[2]))
             self.r.append(torch.Tensor(exp[3]))
             self.n.append(torch.Tensor(exp[4]))
-            self.t.append(torch.Tensor(exp[5]))
-            self.e.append(torch.Tensor(exp[6]))
-            self.done.append(torch.Tensor(exp[7]))
+            self.nn.append(torch.Tensor(exp[5]))
+            self.t.append(torch.Tensor(exp[6]))
+            self.e.append(torch.Tensor(exp[7]))
+            self.done.append(torch.Tensor(exp[8]))
 
         # Trim to make the buffer size < capacity
         while self.__len__() > self.capacity:
@@ -45,6 +47,7 @@ class Buffer():
             self.a.pop(0)
             self.r.pop(0)
             self.n.pop(0)
+            self.nn.pop(0)
             self.t.pop(0)
             self.e.pop(0)
             self.done.pop(0)
@@ -65,6 +68,7 @@ class Buffer():
             torch.cat([self.a[i] for i in ind]), \
             torch.cat([self.r[i] for i in ind]), \
             torch.cat([self.n[i] for i in ind]), \
+            torch.cat([self.nn[i] for i in ind]), \
             torch.cat([self.t[i] for i in ind]), \
             torch.cat([self.e[i] for i in ind]), \
             torch.cat([self.done[i] for i in ind])

swarm/optimizer/edge_optimizer/evo_ppo/core/runner.py
@@ -67,17 +67,17 @@ def rollout_worker(id, type, task_pipe, result_pipe, store_data, model_bucket, e
                 # ---- CALL GAT NETWORK FOR ACTION SELECTION ----
                 if type != 'test':
                     with torch.no_grad():
-                        action, x, attention, logits = net.noisy_action(state, edge_index, active_node_idx, sentence, return_only_action=False, step=steps, pruned_nodes=pruned_nodes)  # Choose an action from the policy network
+                        action, x, attention, logits = net.noisy_action(state, edge_index, active_node_idx, sentence, return_only_action=False, step=steps, pruned_nodes=[])  # Choose an action from the policy network
                 else:
                     with torch.no_grad():
-                        action, x, attention, logits = net.clean_action(state, edge_index, active_node_idx, sentence, return_only_action=False, step=steps, pruned_nodes=pruned_nodes)
+                        action, x, attention, logits = net.clean_action(state, edge_index, active_node_idx, sentence, return_only_action=False, step=steps, pruned_nodes=[])
 
                 # # softmax
                 # action_logits_soft = F.softmax(logits, dim=1)
                 # print("Action logits:", action_logits_soft)
                 
                 # Simulate one step in environment
-                next_state, active_node_idx, reward, done, info = env.step(action.flatten(), record, state, edge_index)
+                next_state, next_active_node_idx, reward, done, info = env.step(action.flatten(), record, state, edge_index)
                 
                 fitness += reward
 
@@ -91,11 +91,12 @@ def rollout_worker(id, type, task_pipe, result_pipe, store_data, model_bucket, e
                     # store the rollout trajectory
                     rollout_trajectory.append([
                         np.array([state_traj]), np.array([next_state_traj]),
-                        np.float32(action_traj), np.float32(np.array([reward])), np.float32(np.array([active_node_idx])),
+                        np.float32(action_traj), np.float32(np.array([reward])), np.float32(np.array([active_node_idx])), np.float32(np.array([next_active_node_idx])),
                         np.float32(np.array([steps])), np.array([edge_index_traj]), np.float32(np.array([float(done)]))
                     ])
                 
                 state = next_state
+                active_node_idx = next_active_node_idx
                 total_frame += 1
                 steps += 1
 

swarm/optimizer/edge_optimizer/evo_ppo/models/discrete_models.py
@@ -14,7 +14,7 @@ from transformers import BertModel, BertTokenizer
 
 # Load pre-trained BERT model and tokenizer
 # Using a distilled BERT model (smaller and faster with fewer parameters)
-bert = BertModel.from_pretrained('distilbert-base-uncased').to('cuda')
+bert = BertModel.from_pretrained('distilbert-base-uncased').to('cuda:7')
 tokenizer = BertTokenizer.from_pretrained('distilbert-base-uncased')
 
 # Freeze BERT model parameters
@@ -65,7 +65,7 @@ class CategoricalGATPolicy(nn.Module):
     def clean_action(self, x: torch.Tensor, edge_index: torch.Tensor, active_node_idx: int, sentence: str, return_only_action=True, step=0, pruned_nodes=[], batch_size=1):
         
         # Encode sentence with BERT
-        inputs = tokenizer(sentence, return_tensors='pt', truncation=True, padding=True).to('cuda')
+        inputs = tokenizer(sentence, return_tensors='pt', truncation=True, padding=True).to('cuda:7')
         with torch.no_grad():
             sentence_embedding = bert(**inputs).last_hidden_state[:, 0, :]  # Use [CLS] token
             sentence_embedding = sentence_embedding.to(x.device)

swarm/optimizer/edge_optimizer/parametrization_ga.py
@@ -43,7 +43,7 @@ class EdgeWiseDistribution(ConnectDistribution):
         # init_tensor = torch.ones(
         #     len(potential_connections)) * init_logit
         # self.edge_logits = torch.nn.Parameter(init_tensor)
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
         node_ids = set([x for pair in potential_connections for x in pair])
         self.node_idx2id = {i: node_id for i, node_id in enumerate(node_ids)}
         self.node_id2idx = {node_id: i for i, node_id in enumerate(node_ids)}